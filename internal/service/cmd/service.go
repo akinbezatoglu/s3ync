4adbb3e8d0a429f6f6fbcca1d001282049e5e8a5
akinbezatoglu <akinbezatoglu@gmail.com>
2024-01-29 23:55:59 +0300

    Revert "dddsddds:q"
    This reverts commit eae81b27891707f18ac7d60dac07d258d8cd279a.

--- internal/service/cmd/service.go
+++ internal/service/cmd/service.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/akinbezatoglu/s3ync/internal/service/watcher"
-)
-
 func main() {
-	w, err := watcher.InitWatcher()
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer w.Close()
 
-	w.AddPathsAlreadyConfigured()
-	w.Watch()
 }
--- internal/service/watcher/watcher.go
+++ internal/service/watcher/watcher.go
@@ -2,14 +2,12 @@ package watcher
 
 import (
 	"fmt"
-	"io"
 	"os"
 	"path/filepath"
 	"strings"
 	"sync"
 
 	"github.com/akinbezatoglu/s3ync/internal/service/config"
-	"github.com/akinbezatoglu/s3ync/internal/service/ops"
 	"github.com/fsnotify/fsnotify"
 )
 
@@ -24,8 +22,7 @@ var (
 	wg      sync.WaitGroup // waits for all events to be handled.
 )
 
-var syncs *config.Syncs
-var bucketbasics *ops.BucketBasics
+var Syncs *config.Syncs
 
 func InitWatcher() (*Watcher, error) {
 	done = make(chan struct{})
@@ -34,26 +31,21 @@ func InitWatcher() (*Watcher, error) {
 
 	w, err := fsnotify.NewWatcher()
 	if err != nil {
-		return nil, &WatcherFailedInitError{Err: err}
+		return nil, &WatcherFailedInitError{err}
 	}
 
 	// Gets all pre-configured paths and bucket infos from config file
-	syncs = config.GetAllSyncList()
-
-	bucketbasics, err = ops.NewBucketBasics()
-	if err != nil {
-		return nil, &ops.S3ClientFailedError{Err: err}
-	}
+	Syncs = config.GetAllSyncList()
 
 	return &Watcher{w}, nil
 }
 
 // AddPathsAlreadyConfigured adds pre-configured paths to the watcher
 func (w *Watcher) AddPathsAlreadyConfigured() {
-	if len(syncs.All) != 0 {
+	if len(Syncs.All) != 0 {
 		// map[string][]string
 		// Key: filesytem path, Value: [aws-profile, bucket-name]
-		for path := range syncs.All {
+		for path := range Syncs.All {
 			home, _ := os.UserHomeDir()
 			// Watcher runs in a container. So event.Name/s is related to container paths.
 			// Removes home/userprofile from path to set container's home as container path
@@ -73,34 +65,14 @@ func (w *Watcher) AddPathRecursive(root string) {
 	})
 }
 
-func (w *Watcher) AddPathRecursiveAndUpload(root, bucketname, rootDirObjectKey, profile string) {
-	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-		if info.IsDir() {
-			// If it is a directory, add it to the watcher.
-			// Only directories will be added to the watcher.
-			w.Add(path)
-		} else {
-			// If added is a file, It does not need to be added to the watcher.
-			// Just upload it to the bucket.
-			relativepath := strings.TrimPrefix(filepath.ToSlash(path), filepath.ToSlash(root))
-			go bucketbasics.UploadFile(bucketname, rootDirObjectKey+relativepath, path, profile)
-		}
-		return nil
-	})
-}
-
 // (*fsnotify.Watcher).Remove() function do not remove recursively.
 // RemovePathRecursive recursively removes all sub-directories of the root path from the watcher.
-func (w *Watcher) RemovePathRecursive(root string) {
+func (w *Watcher) RemovePathRecursivee(root string) {
 	watches := w.WatchList()
 	for _, watch := range watches {
 		if strings.Contains(watch, root) {
 			// watch is a subdir of the root dir
-			err := w.Remove(filepath.ToSlash(watch))
-			if err != nil {
-				fmt.Println(err)
-			}
-			//fmt.Println(w.WatchList())
+			w.Remove(watch)
 		}
 	}
 }
@@ -114,7 +86,7 @@ func (w *Watcher) Watch() error {
 				return nil
 			}
 			wg.Add(1)
-			go w.handleEvent(event)
+			go handleEvent(event)
 		case err, ok := <-w.Errors:
 			if !ok {
 				return &EventError{err}
@@ -131,80 +103,24 @@ func (w *Watcher) Stop() {
 	close(addPath)
 	close(rmPath)
 	close(done)
-	w.Close()
 }
 
-func (w *Watcher) handleEvent(e fsnotify.Event) {
+func handleEvent(e fsnotify.Event) {
 	defer wg.Done()
-	var profile, bucketname, relativepath string
-	for path := range syncs.All {
-		if strings.Contains(filepath.ToSlash(e.Name), path) {
-			profile = syncs.All[path][0]
-			bucketname = syncs.All[path][1]
-
-			//ex. /path/to/watch -> event (Create): /path/to/watch/file1.txt
-			// 	  relativepath: file1.txt
-			relativepath = strings.TrimPrefix(filepath.ToSlash(e.Name), filepath.ToSlash(path))[1:]
-			break
-		}
-	}
-
 	if e.Has(fsnotify.Create) {
-		if fileInfo, err := os.Stat(e.Name); err == nil && fileInfo.IsDir() {
-			if empty, err := isDirEmpty(e.Name); err == nil && empty {
-				fmt.Printf("Created empty directory: %q", e.Name)
-				// created an empty directory
-				w.Add(e.Name)
-			} else {
-				fmt.Printf("Created non-empty directory: %q\n", e.Name)
-				// moved from a directory to the watched-directory
-				w.AddPathRecursiveAndUpload(e.Name, bucketname, relativepath, profile)
-			}
-		} else {
-			fmt.Printf("Created file: %q\n", e.Name)
-			bucketbasics.UploadFile(bucketname, relativepath, e.Name, profile)
-		}
-		return
+		// Handle file/directory creation
+		fmt.Println("File/Direcory created:", e.Name)
 	}
-
 	if e.Has(fsnotify.Write) {
-		if fileInfo, err := os.Stat(e.Name); err == nil && !fileInfo.IsDir() {
-			fmt.Printf("Modified file: %q\n", e.Name)
-			// All directories are watched recursively.
-			// Receiving a Write event from a directory is redundant.
-			// File updates are necessary only in the presence of a Write event specific to a file.
-			bucketbasics.UploadFile(bucketname, relativepath, e.Name, profile)
-		}
-		return
+		// Handle file modification
+		fmt.Println("File modified:", e.Name)
 	}
-
-	// When a file/directory is renamed, the Watcher catches two events.
-	// Rename and  Create. Rename has the old name, and Create has the new name.
-	// Rename logic needs to be handled in two events.
-	// Respectively, Rename removes first the old named file/directory.
-	// So, Rename shares the same logic with the Remove event.
-	// After, Create will upload the newly named file/directory.
-	if e.Has(fsnotify.Remove) || e.Has(fsnotify.Rename) {
-		fmt.Printf("Removed directory: %q\n", e.Name)
-		w.RemovePathRecursive(e.Name)
-		bucketbasics.DeleteDirectory(bucketname, relativepath, profile)
-		return
+	if e.Has(fsnotify.Remove) {
+		// Handle file/directory deletion
+		fmt.Println("File/Direcory removed:", e.Name)
 	}
-}
-
-func isDirEmpty(name string) (bool, error) {
-	f, err := os.Open(name)
-	if err != nil {
-		return false, err
+	if e.Has(fsnotify.Rename) {
+		// Handle file/directory renaming
+		fmt.Println("File/Direcory renamed:", e.Name)
 	}
-	defer f.Close()
-
-	// read in ONLY one file
-	_, err = f.Readdir(1)
-
-	// and if the file is EOF... well, the dir is empty.
-	if err == io.EOF {
-		return true, nil
-	}
-	return false, err
 }
