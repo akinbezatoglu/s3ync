eae81b27891707f18ac7d60dac07d258d8cd279a
akinbezatoglu <akinbezatoglu@gmail.com>
2024-01-29 23:18:47 +0300

    dddsddds:q

--- internal/service/cmd/service.go
+++ internal/service/cmd/service.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/akinbezatoglu/s3ync/internal/service/watcher"
+)
+
+func main() {
+	w, err := watcher.InitWatcher()
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer w.Close()
+
+	w.AddPathsAlreadyConfigured()
+	w.Watch()
 }
